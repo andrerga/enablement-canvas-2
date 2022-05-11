074fabce6bef00a469eaf15592ed9f9fcc0089ed
andrerga <andre.napitupulu@rga.com>
2022-05-11 14:29:38 +0800

    Update live preview

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { CanvasClient } from "@uniformdev/canvas";
-import { Composition, Slot } from "@uniformdev/canvas-react";
+// LESSON 7 - ACTIVITY 4 - START
+import {CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE, CanvasClient,} from "@uniformdev/canvas";
+// LESSON 7 - ACTIVITY 4 - END
+import {Composition, Slot} from "@uniformdev/canvas-react";
 import resolveRenderer from "../lib/resolveRenderer";
 
-export async function getStaticProps() {
+// LESSON 7 - ACTIVITY 4 - START
+export async function getStaticProps({preview}) {
+    // LESSON 7 - ACTIVITY 4 - END
     const client = new CanvasClient({
         apiKey: process.env.UNIFORM_API_KEY,
         projectId: process.env.UNIFORM_PROJECT_ID,
     });
-    const { composition } = await client.getCompositionBySlug({
+    const {composition} = await client.getCompositionBySlug({
         slug: "/",
+        // LESSON 7 - ACTIVITY 4 - START
+        state: preview ? CANVAS_DRAFT_STATE : CANVAS_PUBLISHED_STATE,
+        // LESSON 7 - ACTIVITY 4 - END
     });
 
     console.log(JSON.stringify(composition, null, 4));
@@ -22,23 +29,23 @@ export async function getStaticProps() {
     };
 }
 
-export default function Home({ composition }) {
+export default function Home({composition}) {
     return (
         <Composition data={composition} resolveRenderer={resolveRenderer}>
             <div className={styles.container}>
                 <Head>
                     <title>{composition.parameters.title?.value}</title>
-                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="icon" href="/favicon.ico"/>
                 </Head>
 
                 <main className={styles.main}>
-                    <Slot name="header" />
+                    <Slot name="header"/>
 
-                    <Slot name="body" />
+                    <Slot name="body"/>
                 </main>
 
                 <footer className={styles.footer}>
-                    <Slot name="footer" />
+                    <Slot name="footer"/>
                 </footer>
             </div>
         </Composition>
